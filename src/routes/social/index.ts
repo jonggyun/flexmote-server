9906594fbf618853993005bdb96a0eae6514495f
JongGyun Park <jonggyuni@gmail.com>
2020-03-08 11:05:33 +0900

    Add gihub oauth

--- src/routes/social/index.ts
+++ src/routes/social/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
-import { redirectGoogleLogin, callbackGoogleLogin } from './socialController';
+import {
+  redirectGoogleLogin,
+  callbackGoogleLogin,
+  redirectGithubLogin,
+  callbackGithubLogin,
+} from './socialController';
 
 const router = express.Router();
 
 router.get('/redirect/google', redirectGoogleLogin);
 router.get('/callback/google', callbackGoogleLogin);
+
+router.get('/redirect/github', redirectGithubLogin);
+router.get('/callback/github', callbackGithubLogin);
 
 export default router;
--- src/routes/social/socialController.ts
+++ src/routes/social/socialController.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { google } from 'googleapis';
 import { Response, Request } from 'express';
+import axios from 'axios';
 
 export const redirectGoogleLogin = async (req: Request, res: Response) => {
   try {
@@ -30,8 +32,8 @@ export const callbackGoogleLogin = async (req: Request, res: Response) => {
     const { code } = req.query;
 
     const oauth2Client = new google.auth.OAuth2(
-      process.env.OAUTH_GOOGLE_CLINET_ID,
-      process.env.OAUTH_GOOGLE_CLINET_SECRET,
+      process.env.OAUTH_GOOGLE_CLIENT_ID,
+      process.env.OAUTH_GOOGLE_CLIENT_SECRET,
       process.env.OAUTH_GOOGLE_REDIRECT_URL,
     );
 
@@ -42,6 +44,42 @@ export const callbackGoogleLogin = async (req: Request, res: Response) => {
 
     res.cookie('access_token', access_token);
     res.cookie('id_token', id_token);
+    res.redirect(
+      process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '',
+    );
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export const redirectGithubLogin = (req: Request, res: Response) => {
+  try {
+    const clientId = process.env.OAUTH_GITHUB_CLIENT_ID;
+    const redirectUri = process.env.OAUTH_GITHUB_REDIRECT_URL;
+    const githubOauthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_url${redirectUri}`;
+    res.redirect(githubOauthUrl);
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export const callbackGithubLogin = async (req: Request, res: Response) => {
+  try {
+    const { code } = req.query;
+    const { data } = await axios.post(
+      'https://github.com/login/oauth/access_token',
+      {
+        client_id: process.env.OAUTH_GITHUB_CLIENT_ID,
+        client_secret: process.env.OAUTH_GITHUB_CLIENT_SECRET,
+        code,
+      },
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    );
+    res.cookie('access_token', data.access_token);
     res.redirect(
       process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '',
     );
